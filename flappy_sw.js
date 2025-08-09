534292079d39fdfa748040ac2066ddc922f1c028
MSOslr <mathurinsoler@gmail.com>
2025-08-09 03:49:25 +0200

    Add files via upload

--- flappy_sw.js
+++ flappy_sw.js
@@ -1,5 +1,12 @@
-const CACHE = 'flappy-cache-v1';
-const ASSETS = ['index.html','flappy_manifest.json','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+
+const CACHE = 'flappy-realistic-v1';
+const ASSETS = ['index.html','flappy_manifest.json','flappy_sw.js','icon-192.png','icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
